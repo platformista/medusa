4bdb91496ff4496b25b128dbcefb63bf55f90f4b
Vincenzo Russo <v@artetecha.com>
2023-05-12 17:31:23 +0200

    Upgrade Medusa backend

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -1,57 +1,84 @@
+const dotenv = require("dotenv");
 const config = require("platformsh-config").config();
 
+
+let ENV_FILE_NAME = "";
+switch (process.env.NODE_ENV) {
+  case "production":
+    ENV_FILE_NAME = ".env.production";
+    break;
+  case "staging":
+    ENV_FILE_NAME = ".env.staging";
+    break;
+  case "test":
+    ENV_FILE_NAME = ".env.test";
+    break;
+  case "development":
+  default:
+    ENV_FILE_NAME = ".env";
+    break;
+}
+
+try {
+  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+} catch (e) {}
+
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+const ADMIN_CORS =
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = config.getRoute('storefront').url.slice(0, -1) || "http://localhost:8000";
 
-// Database URL (here we use a local database called medusa-development)
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
-
-// Medusa uses Redis, so this needs configuration as well
+const DATABASE_TYPE = process.env.DATABASE_TYPE || "sqlite";
+const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost/medusa-store";
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
-// Stripe keys
-const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "";
-const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "";
-
-const JWT_SECRET = process.env.JWT_SECRET || "very secure string";
-const COOKIE_SECRET = process.env.COOKIE_SECRET || "very secure string";
-
-// This is the place to include plugins. See API documentation for a thorough guide on plugins.
 const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
-  // Uncomment to add Stripe support.
-  // You can create a Stripe account via: https://stripe.com
+  // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`
   // {
-  //   resolve: `medusa-payment-stripe`,
+  //   resolve: "@medusajs/admin",
+  //   /** @type {import('@medusajs/admin').PluginOptions} */
   //   options: {
-  //     api_key: STRIPE_API_KEY,
-  //     webhook_secret: STRIPE_WEBHOOK_SECRET,
+  //     autoRebuild: true,
   //   },
   // },
-]
+];
 
-module.exports = {
-  projectConfig: {
-    jwt_secret: JWT_SECRET,
-    cookie_secret: COOKIE_SECRET,
-    redis_url: REDIS_URL,
-    // For more production-like environment install PostgresQL
-    database_url: DATABASE_URL,
-    database_type: "postgres",
-    // database_database: "./sql/medusa-db.sql",
-    // database_type: "sqlite",
-    store_cors: STORE_CORS,
-    admin_cors: ADMIN_CORS,
-    // database_extra: { 
-    //   ssl: { 
-    //     rejectUnauthorized: false 
-    //   } 
-    // }
+const modules = {
+  /*eventBus: {
+    resolve: "@medusajs/event-bus-redis",
+    options: {
+      redisUrl: REDIS_URL
+    }
   },
+  cacheService: {
+    resolve: "@medusajs/cache-redis",
+    options: {
+      redisUrl: REDIS_URL
+    }
+  },*/
+}
+
+/** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
+const projectConfig = {
+  jwtSecret: process.env.JWT_SECRET,
+  cookieSecret: process.env.COOKIE_SECRET,
+  database_type: DATABASE_TYPE,
+  store_cors: STORE_CORS,
+  admin_cors: ADMIN_CORS,
+  // Uncomment the following lines to enable REDIS
+  // redis_url: REDIS_URL
+  redis_url: REDIS_URL,
+  // For more production-like environment install PostgresQL
+  database_url: DATABASE_URL,
+}
+
+/** @type {import('@medusajs/medusa').ConfigModule} */
+module.exports = {
+  projectConfig,
   plugins,
+  modules,
 };
--- backend/src/subscribers/README.md
+++ backend/src/subscribers/README.md
@@ -2,24 +2,42 @@
 
 You may define custom eventhandlers, `subscribers` by creating files in the `/subscribers` directory.
 
-```js
-class WelcomeSubscriber {
-  constructor({ welcomeService, eventBusService }) {
-    this.welcomeService_ = welcomeService;
+```ts
+import MyCustomService from "../services/my-custom";
+import { EntityManager } from "typeorm";
+import { OrderService } from "@medusajs/medusa";
+import { IEventBusService } from "@medusajs/types";
 
-    eventBusService.subscribe("order.placed", this.handleWelcome);
+export default class MySubscriber {
+  protected readonly manager_: EntityManager;
+  protected readonly myCustomService_: MyCustomService
+
+  constructor(
+    {
+      manager,
+      eventBusService,
+      myCustomService,
+    }: {
+      manager: EntityManager;
+      eventBusService: IEventBusService;
+      myCustomService: MyCustomService;
+    }
+  ) {
+    this.manager_ = manager;
+    this.myCustomService_ = myCustomService;
+
+    eventBusService.subscribe(OrderService.Events.PLACED, this.handleOrderPlaced);
   }
 
-  handleWelcome = async (data) => {
-    return await this.welcomeService_.sendWelcome(data.id);
-  };
+  handleOrderPlaced = async (data): Promise<any> => {
+    return true;
+  }
 }
 
-export default WelcomeSubscriber;
 ```
 
 A subscriber is defined as a `class` which is registered as a subscriber by invoking `eventBusService.subscribe` in the `constructor` of the class.
 
 The type of event that the subscriber subscribes to is passed as the first parameter to the `eventBusService.subscribe` and the eventhandler is passed as the second parameter. The types of events a service can emmit are described in the individual service.
 
-An eventhandler has one paramenter; a data `object` which contain information relating to the event, including relevant `id's`. The `id` can be used to fetch the appropriate entity in the eventhandler.
+An eventhandler has one parameter; a data `object` which contain information relating to the event, including relevant `id's`. The `id` can be used to fetch the appropriate entity in the eventhandler.
