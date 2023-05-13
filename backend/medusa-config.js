3f9aafc5e833b148f70be017ab864aff644c147d
Vincenzo Russo <v@artetecha.com>
2023-05-13 16:16:28 +0200

    Try reformatting

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -35,13 +35,11 @@ const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost/medusa-st
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
 const plugins = [
-    `medusa-fulfillment-manual`,
-    `medusa-payment-manual`,
-    {
-      resolve: "@medusajs/admin",
-      /** @type {import('@medusajs/admin').PluginOptions} */
-      options: {},
-    },
+  `medusa-fulfillment-manual`,
+  `medusa-payment-manual`,
+  {
+    resolve: "@medusajs/admin",
+  },
 ];
 
 const modules = {
